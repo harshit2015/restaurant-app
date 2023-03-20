0ed72e2f8c887fe24faa34e7c91886639b315768
Harshit Dixit <harshit.dixit01@nagarro.com>
2023-03-20 17:58:21 +0530

    Added inventory and header components

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HeaderComponent } from './header/header.component';
+import { InventoryComponent } from './inventory/inventory.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'header', component: HeaderComponent },
+  { path: 'menu', component: InventoryComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
